0ba29aa56bfc369b9df8cf7774f638a81b46eb79
ivex0002 <garam199803@gmail.com>
2026-03-31 16:44:18 +0900

    중간저장 : uuid 설치 중 npm 오류 발생

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import { MymoduleModule } from './mymodule/mymodule.module';
 
 @Module({
   // 가져오기
-  imports: [],
+  imports: [BoardModule, MymoduleModule],
   // 내보내기
   exports: [],
   // 엔드포인트
-  controllers: [AppController],
+  controllers: [],
   // 실제 로직
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
 
---
+++ src/board/board.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
+
+@Injectable()
+export class BoardService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(newBoard: Omit<Board, 'id'>) {}
+}
