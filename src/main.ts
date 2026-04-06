bcb2d5c7f33f5255d9eed03f7276f1d728d7a1b5
ivex0002 <garam199803@gmail.com>
2026-04-06 15:20:35 +0900

    포트 설정 적용 완료

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import config from 'config';
 
-const DEFAULT_PORT = 4001;
+// const DEFAULT_PORT = 4001;
 
-const config = new DocumentBuilder()
+export interface ServerConfig {
+  port: number;
+}
+
+const swaggerConfig = new DocumentBuilder()
   .setTitle('Auth API')
   .setDescription('API docs')
   .setVersion('1.0')
@@ -14,12 +19,18 @@ const config = new DocumentBuilder()
 
 async function bootstrap() {
   const logger = new Logger('main');
+
+  if (!config.has('server')) {
+    throw new Error('Configuration "server" is missing in yaml file');
+  }
+  const serverConfig = config.get<ServerConfig>('server');
+
   const app = await NestFactory.create(AppModule);
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  logger.log(`process.env.PORT : ${process.env.PORT}`);
-  const port = process.env.PORT ?? DEFAULT_PORT;
+  const port = serverConfig.port;
+  logger.log(`process.env.PORT : ${port}`);
   await app.listen(port);
   logger.log(`app running on port ${port}`);
 }
